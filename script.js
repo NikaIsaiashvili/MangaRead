26f0adfafdb8f75aaeedcb59685aad6a5eaa9c74
NikaIsaiashvili <nisaiashvili@gmail.com>
2025-07-15 21:09:11 +0400

    fix header and search
    responsive, fixed with flexbox

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 "use strict";
 const toggle = document.getElementById("toggle");
+const searchButton = document.getElementById("search-button");
 
 document.addEventListener("DOMContentLoaded", () => {
   toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     const isDark = document.body.classList.contains("dark");
     toggle.textContent = isDark ? "☀️" : "🌙";
+  });
+
+  searchButton.addEventListener("click", () => {
+    const searchBox = document.getElementById("search-box");
+    searchBox.classList.toggle("search-box-hide");
   });
 });
